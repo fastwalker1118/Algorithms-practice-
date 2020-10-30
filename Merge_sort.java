61695093d3c970b1f4577dca0900e9c15ffd144a
fastwalker1118 <66637231+fastwalker1118@users.noreply.github.com>
2020-10-30 15:50:22 -0400

    Add files via upload

--- Merge_sort.java
+++ Merge_sort.java
@@ -1,5 +1,6 @@
 /*The difference between if and else if 
- *for else if, if the first if is true, all of the else if s won't be executed,*/
+ *for else if, if the first if is true, all of the else if s won't be executed,
+ */
 
 public class Merge_sort {
 
@@ -13,54 +14,71 @@ public class Merge_sort {
 	}
 	
 	
-	public static void merge(int[] a, int[] aux, int lo, int mid, int hi) {
+	public static void merge(int[] a, int[] aux, int lo, int midd, int hi) {
 		
 		for (int k = lo; k <= hi; k++) {
 			aux[k] = a[k];  
 		}
 		
-		int i = lo, j = mid+1; 
+		int i = lo, j = midd+1; 
 		
 		for (int k = lo; k <= hi; k++) {
-			if (i > mid) {
-				a[k] = aux[j++];
+			if (i > midd) {
+				a[k] = aux[j++];  
 			}
-			else if (j > hi) {  // else if not if
+			else if (j > hi) {
 				a[k] = aux[i++];
 			}
-			else if (less(aux[j], aux[i])) {  // else if not if
+			else if (less(aux[j], aux[i])) { 
 				a[k] = aux[j++]; 
 			}
 			else {
 				a[k] = aux[i++];
 			}
+			/*for (int io = 0; io < a.length; io++) {
+				System.out.print(a[io] + "   ");
+			}
+			System.out.println();
+			for (int io = 0; io < a.length; io++) {
+				System.out.print(aux[io] + "   ");
+			}
+			System.out.println();*/
 		}
 		
 	}
 	
 	public static void sorting(int[] a, int[] aux, int lo, int hi) {
 		
-		if (hi <= lo) {
-			return;
+		if (hi <= lo) {  
+			return; 
+			// base case for the recursion later
 		}
+		// we use index variables lo, midd, hi to "divide and conquer"
+		// the original value is low = 0, mid = 5, high = 10
+		int midd = lo + (hi - lo) / 2; 
+		sorting (a, aux, lo, midd); 
+		sorting (a, aux, midd+1, hi); 
 		
-		int midd = lo + (hi - lo) / 2;
-		sorting (a, aux, lo, midd); // not sorting (aux, a, lo, midd);
-		sorting (a, aux, midd+1, hi);
-		merge(a, aux, lo, midd, hi);
+		/*System.out.println(lo);
+		System.out.println(midd);
+		System.out.println(hi);
+		System.out.println(); */
+		
+		merge(a, aux, lo, midd, hi); 
+		
 	}
 	
 	public static void sort(int[] a) {
-		int[] aux = new int[a.length];
-		sorting(a,aux,0,a.length-1);
+		int[] aux = new int[a.length]; // creates an auxiliary array with the same length
+		sorting(a,aux,0,a.length-1); // passes the array and the auxiliary to sorting method with the first and last index number
 	}
 	
 	public static void main(String[] args) {
 		
-		int[] a = {80, 16, 70, 56, 52, 62, 98, 30, 79, 55, 55};
-		sort(a);
-		for (int i = 0; i < a.length; i++) {
-			System.out.print(a[i] + "   ");
+		int[] a = {80, 16, 70, 56, 52, 98, 30,  79};
+		sort(a); // passes the array as an argument to sort method
+		for (int i = 0; i < a.length; i++) { 
+			System.out.print(a[i] + "   ");  
 		}
 			
 	}
