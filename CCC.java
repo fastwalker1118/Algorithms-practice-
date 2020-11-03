a2adb8a70e212da71d22ce2307436c7347f0a507
fastwalker1118 <66637231+fastwalker1118@users.noreply.github.com>
2020-11-02 19:13:44 -0500

    Add files via upload

---
+++ CCC.java
@@ -0,0 +1,35 @@
+import java.util.*; 
+
+public class CCC {
+	
+	/*public static void main(String[] args) {
+		
+		ArrayList<Double> al = new ArrayList<Double>();
+		Scanner s = new Scanner(System.in);
+		int number = s.nextInt();
+		for (int i = 0; i < number; i++) {
+			double temp = s.nextInt();
+			al.add(temp);
+		}
+		ArrayList<Double> a = new ArrayList<Double>();
+		Collections.sort(al); 
+		for(int i = 1; i < al.size(); i++) {
+			double temp = (al.get(i) - al.get(i-1))/2;
+			a.add(temp);
+		}
+		double last = 0;
+		for(int i = 1; i < a.size(); i++) {
+			double temp = a.get(i)+a.get(i-1);
+			if (i == 1) {
+				last = temp;
+			}
+			else{
+				if(temp<last) {
+					last = temp;
+				}
+			}
+		}
+		System.out.print(last);
+	}*/
+	
+}
